3c769b8d9ca1618b4c73e3cc05e81a3f82d676e2
Jardes Souza <jardessouza@gmail.com>
2023-04-13 13:49:35 -0300

    Tecnico create finalizando excessoes e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico-create/tecnico-create.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 
 
@@ -79,6 +80,7 @@ import { TecnicoCreateComponent } from './components/tecnico-create/tecnico-crea
       closeButton: true,
       progressBar: true
     }),
+    NgxMaskModule.forRoot()
   ],
   providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
